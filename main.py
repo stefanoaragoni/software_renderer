a4c6f78b635c8e0d7a78107d722dcb169c88fafc
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-09-12 16:30:54 -0600

    TREX

--- main.py
+++ main.py
@@ -7,33 +7,32 @@ def glpoint():
 
     r.glCreateWindow(500,500)
 
-    r.glClearColor(0,0,0) #parametros en rango de 0 a 1
+    r.glClearColor(0,0.7098,0.8862)
     r.glClear()
 
     r.glViewPort(0,0,500,500) 
 
-    r.glColor(1,1,1) #parametros en rango de 0 a 1
+    r.glColor(1,1,1)
     
     r.lookAt(eye=[0,0,1],center=[0,0,0],up=[0,1,0])
 
-    r.stars()
-
     #------------------ORIGINAL
+    """modelo 1 (10/50)"""
+    t.read('./models/trex.bmp')
+    r.active_texture = t
     r.active_shader = r.shader
-    r.glLoad('./models/sphere.obj', translate=(-1,-1,0), scale=(5,5,5), rotate=(0,0,0))
+    r.glLoad('./models/trex.obj', translate=(-1,-4,0), scale=(3,3,3), rotate=(0,100,0), texture=t)
     r.draw('TRIANGLES')
 
-    r.active_shader = r.shader2
-    r.glLoad('./models/Ring.obj', translate=(-1,-1.3,0), scale=(3.3,2,3.3), rotate=(0,0,0))
-    r.draw('RANDOM')
+    """modelo 1 (10/50)"""
+    t.read('./models/dino.bmp')
+    r.active_texture = t
+    r.active_shader = r.shader
+    r.glLoad('./models/dino.obj', translate=(-1,-4,0), scale=(0.05,0.05,0.05), rotate=(0,100,0), texture=t)
+    r.draw('TRIANGLES')
 
-    r.active_shader = r.shader2
-    r.glLoad('./models/Ring.obj', translate=(-0.95,-1.35,0), scale=(3.3,2,3.3), rotate=(0,0,0))
-    r.draw('RANDOM')
+    
 
-    r.active_shader = r.shader2
-    r.glLoad('./models/Ring.obj', translate=(-1.05,-1.25,0), scale=(3.3,2,3.3), rotate=(0,0,0))
-    r.draw('RANDOM')
 
     r.glFinish()
 
